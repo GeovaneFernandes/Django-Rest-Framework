c3a5dd4b22b7ce7003ff619b3ffe55d198530177
Geovane Fernandes <geovanefernandesdois@gmail.com>
2025-08-19 14:37:43 -0300

    Update settings: allow all hosts and change language code to Portuguese

--- DjangoRestFramework/settings.py
+++ DjangoRestFramework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3ji(!-=es@*nb3o$sh#7qf3rl5l9sgz!#_kk+$c=+b)89)#nep
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
