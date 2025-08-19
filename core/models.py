9759236aa17d2736eeb186b3490533f6e5611e06
Geovane Fernandes <geovanefernandesdois@gmail.com>
2025-08-19 16:07:14 -0300

    Add Cliente model with fields nome, sobrenome, and email

--- core/models.py
+++ core/models.py
@@ -6,3 +6,9 @@ class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
     estoque = models.IntegerField('Estoque')
+
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('Email', max_length=100)
