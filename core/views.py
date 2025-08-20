c344fd2c07b671f19320a849c027a6928620dd11
Geovane Fernandes <geovanefernandesdois@gmail.com>
2025-08-20 15:30:00 -0300

    Add product detail view and template; update index to link to products

--- core/views.py
+++ core/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-
+from .models import Produto
 # Create your views here.
 
 def index(request):
+    produtos = Produto.objects.all()
     context = {
-        'curso': 'Django Framework'
+        'curso': 'Django Framework',
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)
