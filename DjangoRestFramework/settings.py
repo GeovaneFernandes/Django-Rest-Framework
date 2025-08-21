9cd3cf07fdfb2c8a83e56a921717cf1ca0cce2f8
Geovane Fernandes <geovanefernandesdois@gmail.com>
2025-08-21 10:25:58 -0300

    Update settings for production: set DEBUG to False and add WhiteNoise middleware; enhance produto.html with static file loading

--- DjangoRestFramework/settings.py
+++ DjangoRestFramework/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ji(!-=es@*nb3o$sh#7qf3rl5l9sgz!#_kk+$c=+b)89)#nep'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
